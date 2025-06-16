551b730dcceb535272ca6584ad8bf4867a6d0036
Maria Eduarda Fodor <130316981+fod0rr@users.noreply.github.com>
2025-06-16 20:37:48 -0300

    Update Book.java

--- CBTSWE1-TP03/src/models/Book.java
+++ CBTSWE1-TP03/src/models/Book.java
@@ -1,6 +1,6 @@
 package models;
 
-//Eduardo Miranda CB3026604 | Cauã Barros da Costa CB3025179
+//Maria Eduarda Fodor CB3025063 | Pedro Xavier Oliveira CB3027376
 
 public class Book {
 	protected int id;
